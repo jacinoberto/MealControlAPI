851c5cba59cd2407f5e31d214eb84621c97aa38b
Jacimara Noberto <estudo.jacimaranoberto@gmail.com>
2024-06-26 21:07:45 -0300

    Definindo contexto da aplicação

---
+++ noberto.mealControl.Infra.Database/Context/MealControlDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using noberto.mealControl.Core.Entities;
+
+namespace noberto.mealControl.Infra.Database.Context;
+
+public class MealControlDbContext : DbContext
+{
+    public MealControlDbContext(DbContextOptions<MealControlDbContext> options) 
+    : base(options) {}
+
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Administrator> Administrators { get; set; }
+    public DbSet<Manager> Managers { get; set; }
+    public DbSet<Worker> Workers { get; set; }
+    public DbSet<Work> Works { get; set; }
+    public DbSet<TeamManagement> TeamManagement { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<ScheduleEvent> ScheduleEvents { get; set; }
+    public DbSet<ScheduleLocalEvent> ScheduleLocalEvents { get; set; }
+    public DbSet<Meal> Meals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(MealControlDbContext).Assembly);
+    }
+}
